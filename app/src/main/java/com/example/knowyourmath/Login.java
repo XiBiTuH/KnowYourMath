a5c43d1e40ee4c5e1ba07d3aaac0ba5a7fe04632
jorge.brito@ubi.pt <kennysthegod503>
2019-11-10 21:38:08 +0000

    Register Login UI Improvements

---
+++ app/src/main/java/com/example/knowyourmath/Login.java
@@ -0,0 +1,15 @@
+package com.example.knowyourmath;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
